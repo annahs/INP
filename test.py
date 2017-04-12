18b3e0c07f32ca301943412682c54219c3289570
Sarah <mcallister@McAllisters-MacBook-Pro.local>
2017-04-11 22:10:18 -0700

    revised footprint analysis

--- test.py
+++ test.py
@@ -1,24 +1,44 @@
 import os
 import sys
 from pprint import pprint
-lines = []
-file = '/Users/mcallister/projects/INP/INP/Alert_10d_parameters.txt'
-i = 1
-with open(file, 'r') as f:
-	for line in f:
-		newline = line.split()
-		if newline[0].startswith('hours'):
-			a= newline[1]
-		if newline[0] == 'desert':
-			b= newline[3]
-		if newline[0] == 'sea':
-			c= newline[4]
-		if newline[0] == 'open':
-			d= newline[4]
-		if i%7 == 0:	
-			lines.append([a,b,c,d])
-		i+=1
-	pprint(lines)
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import pickle
+import INP_source_apportionment_module as INPmod
+
+lons = [4.2,5.5,6.5]
+lats = [1.5,2.5,3.5]
+
+lon_bins = np.linspace(0, 10, 5+1)
+lat_bins = np.linspace(0, 5,  5+1)
+
+# Histogram the lats and lons to produce an array of frequencies in each box.
+# Because histogram2d does not follow the cartesian convention 
+# (as documented in the np.histogram2d docs)
+# we need to provide lats and lons rather than lons and lats
+density, _, _ = np.histogram2d(lats, lons, [lat_bins, lon_bins])
+density2, _, _ = np.histogram2d(lats, lons, [lat_bins, lon_bins])
+
+# Turn the lon/lat bins into 2 dimensional arrays ready 
+# for conversion into projected coordinates
+lon_bins_2d, lat_bins_2d = np.meshgrid(lon_bins, lat_bins)
 
 
+print lon_bins_2d
+print lat_bins_2d
+print density
 
+pprint(np.dstack((density,density2)))
+
+i=0
+for row in np.dstack((density,density2)):
+	j=0
+	for d1,d2 in row:
+		if d1 >0:
+			print i,j,'**'
+			print lon_bins_2d[0][j]
+			print lat_bins_2d[i][0]
+		j+=1	
+	i+=1
